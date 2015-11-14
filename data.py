4629a7c854334eb8c5b5c4c36cc929b4c26862c4
Kelvin Liu <kelvinliu@Kelvins-MacBook-Air.local>
2015-11-14 16:25:18 -0500

    finished naive haiku generation

--- data.py
+++ data.py
@@ -8,11 +8,20 @@ import sys
 
 PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
+#wans
 try:
     with open(os.path.join(PATH, 'wans/cue_target_pairs.json')) as fin:
         wan_graph = json.load(fin)
 except IOError as e:
     print('cue_target_pairs.json not found -- please generate with wan_json.sh')
+    sys.exit(1)
+
+#ngrams
+try:
+    with open(os.path.join(PATH, 'coca/ngram_pos_map.json')) as fin:
+        ngram_pos = json.load(fin)
+except IOError as e:
+    print('ngram_pos_map.json not found -- please generate with clean_grams.py')
     sys.exit(1)
 
 def wan_walk(seed, steps=3):
@@ -32,9 +41,22 @@ def get_associations(seed, size=8):
     else:
         return None
 
-def match(): pass
+def get_ngram(word):
+    """Returns an ngram which contains the given word."""
+    candidates = []
+    for k in ngram_pos.keys():
+        if word in set(k.split(' ')):
+            candidates.append(k)
+    return sample(candidates, 1)[0]
+
+def match(seed, words):
+    """Returns a haiku given a seed and related words by matching ngrams"""
+    haiku = [get_ngram(seed)]
+    for word in sample(words, 2):
+        haiku.append(get_ngram(word))
+    return "/".join(haiku)
 
 def generate(seed):
     """Returns a haiku based on a seed list"""
     words = get_associations(seed)
-    return ', '.join(words) if words is not None else None
+    return match(seed, words) if words is not None else None
