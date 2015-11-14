2406cd5efc7692a5fcd5d1fdd35bc543c780a4e7
Kelvin Liu <kelvinliu@Kelvins-MacBook-Air.local>
2015-11-14 08:43:19 -0500

    added conversion to utf-8 after download

--- data/wans/download.sh
+++ data/wans/download.sh
@@ -2,5 +2,5 @@
 
 url_base=http://w3.usf.edu/FreeAssociation/AppendixA/Cue_Target_Pairs.
 for letters in {A-B,C,D-F,G-K,L-O,P-R,S,T-Z}; do
-    curl -o $letters.txt $url_base$letters
+    curl $url_base$letters | iconv -f iso-8859-1 -t utf-8 > $letters.txt
 done
