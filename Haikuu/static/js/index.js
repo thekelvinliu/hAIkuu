e3b37556b598c5b0259bcd20f9ee68b8e221ca37
Henry Du <h7du@uwaterloo.ca>
2015-11-14 22:50:48 -0500

    changed token again

--- Haikuu/static/js/index.js
+++ Haikuu/static/js/index.js
@@ -20,7 +20,7 @@ var createHaiku = function() {
             $("#loading").show();
             // Base64DataURL
             $.ajax({
-              headers: {authorization: "Bearer 2BKdevklbfTHmeOa41YP7nksnIPcaL"},
+              headers: {authorization: "Bearer brwV2MxCkjH7J5Jai8gZ8JaxIHCdWT"},
               url: "https://api.clarifai.com/v1/tag",
               type: "post",
               data: {encoded_image:base64Img.substring(base64Img.indexOf(",") + 1)}
