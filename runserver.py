bff959158230f4b598ecce5ea59949c687f4765b
thekelvinliu <kelvin@thekelvinliu.com>
2016-01-24 13:31:34 -0600

    ensure port given is an int

--- runserver.py
+++ runserver.py
@@ -5,4 +5,4 @@ from Haikuu import app as application
 
 if __name__ == "__main__":
     application.debug = True
-    application.run(host='0.0.0.0', port=environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
