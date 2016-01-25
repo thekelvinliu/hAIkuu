41ee7e517aa2a117570bce42c2e1ceb5f99551a5
thekelvinliu <kelvin@thekelvinliu.com>
2016-01-24 22:18:52 -0600

    adjusted to reflect new file structure

--- Haikuu/data.py
+++ Haikuu/data.py
@@ -3,59 +3,73 @@
 from __future__ import print_function
 import json
 import os.path
-from random import sample
+import random
 import sys
 
-PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+#absolute path to root Haikuu package
+P_DIR = os.path.dirname(os.path.realpath(__file__))
+GRAPH_FILE = 'cue_targets_mapping.json'
+NGRAM_FILE = 'clean_ngrams.json'
 
-#wans
+#load word graph
 try:
-    with open(os.path.join(PATH, 'wans/cue_target_pairs.json')) as fin:
-        wan_graph = json.load(fin)
-        print("graph loaded")
+    with open(os.path.join(P_DIR, GRAPH_FILE)) as fin:
+        word_graph = json.load(fin)
+        print(GRAPH_FILE, "loaded!")
 except IOError as e:
-    print('cue_target_pairs.json not found -- please generate with wan_json.sh')
+    print(GRAPH_FILE, "not found -- please generate with `generate_map.sh`")
     sys.exit(1)
 
-#ngrams
+#load ngrams
 try:
-    with open(os.path.join(PATH, 'coca/short.txt')) as fin:
-        ngrams = [line.split(' ') for line in json.load(fin)]
-        print("ngrams loaded")
+    with open(os.path.join(P_DIR, NGRAM_FILE)) as fin:
+        ngrams = json.load(fin)
+        for i in range(len(ngrams)):
+            ngrams[i].append(set(ngrams[i][0].split(' ')))
+        print(NGRAM_FILE, "loaded!")
 except IOError as e:
-    print('short.txt not found -- please generate with clean_grams.py')
+    print(NGRAM_FILE, "not found -- please generate with `clean_ngrams.sh`")
     sys.exit(1)
 
-def wan_walk(seed, steps=3):
-    """Returns a word related to the seed word."""
+def generate(seed):
+    """Returns a full haiku based on a seed word."""
+    topics = get_associations(seed)
+    if topics is None:
+        return None
+    ngram = get_ngram(seed).replace("n't", 'not')
+    if ngram is None:
+        return None
+    haiku = [ngram]
+    i = 2
+    while i > 0 and len(topics) > 0:
+        random.shuffle(topics)
+        ngram = get_ngram(topics.pop()).replace("n't", 'not')
+        if ngram is not None:
+            haiku.append(ngram)
+            i -= 1
+    if len(haiku) < 3:
+        return None
+    else:
+        return " / ".join(haiku)
+
+def get_associations(seed, size=8):
+    """Returns a list of size words related to a seed word."""
+    if word_graph.get(seed) is not None:
+        return [random_walk(seed) for i in range(size)]
+    else:
+        return None
+
+def random_walk(seed, steps=3):
+    """Returns related to seed by taking a random walk on the graph"""
     retval = seed
     while steps > 0:
-        word = sample(wan_graph[retval], 1)[0]
-        if wan_graph.get(word) is not None:
+        word = random.choice(word_graph[retval])
+        if word_graph.get(word) is not None:
             retval = word
             steps -= 1
     return retval
 
-def get_associations(seed, size=8):
-    """Returns a list of size words related to a seed word."""
-    if wan_graph.get(seed) is not None:
-        return [wan_walk(seed) for i in range(size)]
-    else:
-        return None
-
 def get_ngram(word):
-    """Returns an ngram which contains the given word."""
-    candidates = [" ".join(g) for g in ngrams if word in g]
-    return sample(candidates, 1)[0]
-
-def match(seed, words):
-    """Returns a haiku given a seed and related words by matching ngrams"""
-    haiku = [get_ngram(seed)]
-    for word in sample(words, 2):
-        haiku.append(get_ngram(word))
-    return "/".join(haiku)
-
-def generate(seed):
-    """Returns a haiku based on a seed list"""
-    words = get_associations(seed)
-    return match(seed, words) if words is not None else None
+    """Returns an ngram containing the given word."""
+    candidates = [g[0] for g in ngrams if word in g[-1]]
+    return random.choice(candidates) if candidates else None
