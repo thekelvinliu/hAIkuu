f8af11de50dd5b66fa12abfaa91b48767b7f1f53
Kelvin Liu <kelvinliu@kelvinliu.local>
2016-02-19 15:33:32 +0000

    restructured code for better import support

--- coca/clean_ngrams.py
+++ coca/clean_ngrams.py
@@ -10,21 +10,35 @@ DL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'downloads')
 OUTPUT_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                            'clean_ngrams.json')
 
-#eventual json object
-data = []
-#item to split on
-split = 2
-#iterate over all text files and add to data dict
-for f in listdir(DL_DIR):
-    print("cleaning", f)
-    with open(os.path.join(DL_DIR, f)) as fin:
-        for i, line in enumerate(fin):
-            lst = line.strip().split('\t')[1:]
-            k = " ".join(lst[:split])
-            v = lst[split:]
-            data.append([k, v[0], v[-1]])
-    split += 1
+def ngram_hash(ngram_data):
+    """returns the hashed value of a piece of ngram data"""
+    return hash(''.join(ngram_data))
 
-with open(OUTPUT_FILE, 'w') as fout:
-    json.dump(data, fout)
-print("done", OUTPUT_FILE)
+def main():
+    #keep track of seen ngrams
+    seen = set()
+    #eventual json object
+    data = []
+    #item to split on
+    split = 2
+    #iterate over all text files and add to data dict
+    for f in listdir(DL_DIR):
+        print("cleaning", f)
+        with open(os.path.join(DL_DIR, f)) as fin:
+            for i, line in enumerate(fin):
+                lst = line.strip().split('\t')[1:]
+                k = " ".join(lst[:split])
+                v = lst[split:]
+                ngram_data = [k, v[0], v[-1]]
+                h = ngram_hash(ngram_data)
+                if h not in seen:
+                    data.append(ngram_data)
+                    seen.add(h)
+        split += 1
+    #write json file
+    with open(OUTPUT_FILE, 'w') as fout:
+        json.dump(data, fout)
+    print("done", OUTPUT_FILE)
+
+if __name__ == '__main__':
+    main()
